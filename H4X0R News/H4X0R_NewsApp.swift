cffeb4c0d437cdf6bb282baf606ada0519171ce5
marianul-commits <80788036+marianul-commits@users.noreply.github.com>
2023-03-27 22:34:02 +0300

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Marian Nasturica on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
